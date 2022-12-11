db6edf9c056c0f063911e8f83c0d9908dcf3842a
Juan Bello <36510813+Rastajuan@users.noreply.github.com>
2022-12-12 00:05:36 +0100

    ...

--- js/conFetch.js
+++ js/conFetch.js
@@ -1,56 +1,45 @@
-
+var boton1 = document.getElementById("1");
+const CAJATEXTOS = document.getElementById("salidaTexto");
 /* Se añade un evento a cada botón para que al pulsarlos se ejecute la función*/
-document.getElementById("1").addEventListener("click", seleccionarTexto);
-document.getElementById("2").addEventListener("click", seleccionarTexto);
-document.getElementById("3").addEventListener("click", seleccionarTexto);
+boton1.addEventListener("onclick", seleccionarTextoExterno);
+document.getElementById("2").addEventListener("click", seleccionarTextoExterno);
+document.getElementById("3").addEventListener("click", seleccionarTextoExterno);
 
 /* Función que selecciona el texto segun pulsemos un boton u otro */
-function seleccionarTexto(e) {
-	/* Se crea un switch para seleccionar el texto segun el boton pulsado. */
-	switch (parseInt(e.target.value)) {
-		case 1:
-			// Se crea la petición al servidor
-			fetch("archivoExterno/texto1.txt")
-				/* Se obtiene la respuesta del servidor mediante el uso del objeto
-                then ya que FETCH trabaja con PROMESAS. */
-				.then( (res)=> {
-					// Se obtiene el texto del archivo
-					return res.text();
-					// Se ejecuta la función que muestra el texto
-				})
-				.then((data)=> {
-					// Se muestra el texto en el párrafo
-					document.getElementById("salidaTexto").innerHTML = data;
-				});
-			break;
-		case 2:
-			fetch("archivoExterno/texto2.txt")
-				.then((res) => {
-					// Se obtiene el texto del archivo
-					return res.text();
-					/* Se ejecuta la función que muestra el texto mediante 
-                el uso del objeto then  utilizando la nomenclatura de arrow function 
-                para declarar la función */
-				})
-				.then((data) => {
-					// Se muestra el texto en el párrafo
-					document.getElementById("salidaTexto").innerHTML = data;
-				});
-			break;
-		case 3:
-			fetch("archivoExterno/texto3.txt")
-				.then((res) => {
-					return res.text();
-				})
-				.then((data) => {
-					document.getElementById("salidaTexto").innerHTML = data;
-				});
-			break;
-		
+function seleccionarTextoExterno(e) {
+
+	if (e.target.value == 1) {
+		fetch("archivoExterno/texto1.txt")
+			.then((res) => {
+				return res.text();
+			})
+			.then((contenido) => {
+				CAJATEXTOS.innerHTML = contenido;
+			});
+	}
+	else
+		if (e.target.value == 2) {
+		fetch("archivoExterno/texto2.txt")
+			.then((res) => {
+				return res.text();
+			})
+			.then((contenido) => {
+				CAJATEXTOS.innerHTML = contenido;
+			});
+		}
+		else
+			if (e.target.value == 3) {
+		fetch("archivoExterno/texto3.txt")
+			.then((res) => {
+				return res.text();
+			})
+			.then((contenido) => {
+				CAJATEXTOS.innerHTML = contenido;
+			});
 	}
 }
 
+/* Función que oculta el texto */
 function ocultarTexto() {
-	ocultar = document.getElementById("salidaTexto");
-	ocultar.innerHTML = "";
+	CAJATEXTOS.innerHTML = "";
 }
