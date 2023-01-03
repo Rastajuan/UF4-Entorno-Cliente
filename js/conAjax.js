1dc7cd81e66f46b62f1118d6439295ff432f55d2
Juan Bello <36510813+Rastajuan@users.noreply.github.com>
2023-01-03 23:36:50 +0100

    Update conAjax.js

--- js/conAjax.js
+++ js/conAjax.js
@@ -8,7 +8,7 @@ MOSTRAR.addEventListener("click", mostrarTexto);
 OCULTAR.addEventListener("click", ocultarTexto);
 
 
-//Creamos el objeto XMLHttpRequest mediante una funcion. Es siempre el primer paso para trabajar con AJAX. XMLHttpReques no permite  crear varias peticones al servidor al mismo tiempo. Si se quiere crear varias peticiones, hay que crear varios objetos XMLHttpRequest. Para crear el objeto XMLHttpRequest, hay que comprobar si el navegador soporta el objeto XMLHttpRequest. Si no lo soporta, se intenta crear con ActiveX. Los navegadores que no soportan ActiveX no podran usar AJAX
+//Creamos el objeto XMLHttpRequest mediante una funcion. Es siempre el primer paso para trabajar con AJAX. XMLHttpReques no permite  crear varias peticiones al servidor al mismo tiempo. Si se quiere crear varias peticiones, hay que crear varios objetos XMLHttpRequest. Para crear el objeto XMLHttpRequest, hay que comprobar si el navegador soporta el objeto XMLHttpRequest. Si no lo soporta, se intenta crear con ActiveX. Los navegadores que no soportan ActiveX no podran usar AJAX
 
 function creaObjetoXMLHttpRequest()
 {	//Variable que contendra el objeto XMLHttpRequest. Al principio se le asigna false para que no de error si el navegador no soporta el objeto XMLHttpRequest
@@ -38,14 +38,14 @@ docTxt.open("GET", "archivoExterno/texto.txt", true);
 //Funcion que recoge la respuesta del archivo. Si readyState es 4 y status es 200, el archivo se ha cargado correctamente
 function leerArchivo() {
 	if (docTxt.readyState == 4 && docTxt.status == 200) {  
-		texto = docTxt.responseText; //Asignamos el texto del archivo a la variable texto que usaremos en las funciones mostrarTexto() y ocultarTexto()
+		texto = docTxt.responseText; //Asignamos el texto del archivo a la variable texto que usaremos en las funciones mostrarTexto() y ocultarTexto(). responseText es una propiedad que contiene el texto del archivo que se ha cargado en el objeto XMLHttpRequest 
 	}
 }
 
-//Recogemos la respuesta del archivo. No ponemos parentesis porque no queremos que se ejecute la funcion, sino que se ejecute cuando se produzca el evento
+//Recogemos la respuesta del archivo. No ponemos parentesis porque no queremos que se ejecute la funcion, sino que se ejecute cuando se produzca el evento. Onreadystatechange se ejecuta cada vez que readyState cambia de valor y status cambia de valor o se produce un error en la peticion al servidor  
 docTxt.onreadystatechange = leerArchivo; 
 
-//Enviamos la peticion al servidor
+//Enviamos la peticion al servidor. Si no se pone esta linea, la peticion no se envia al servidor y no se cargara el archivo externo.La peticion se envia al servidor cuando se ejecuta la funcion leerArchivo(). send() es una funcion que se ejecuta cuando se produce el evento onreadystatechange
 docTxt.send(); 
 
 
